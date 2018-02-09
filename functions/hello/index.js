5db2de93e2875c2c7a7c21772724a9cc67ae4f39
Matt Boldt <me@mattboldt.com>
2018-02-09 17:55:32 -0600

    added index.json

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,16 +1,21 @@
+let fs = require("fs");
+let path = require("path");
+
 console.log('starting function')
-exports.handle = (e, ctx, callback) => {
+exports.handle = (e, context, callback) => {
   console.log('processing event: %j', e);
-  const body = {
-    title: 'Michael Scott oh god no',
-    url: 'https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif',
-    tags: ['oh god no']
-  };
-  const res = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify(body)
-  };
-  callback(null, res);
+  let body = {}
+
+  fs.readFile(__dirname + '/index.json', function (err, data) {
+    if (err) throw err;
+    body = JSON.parse(data);
+
+    const res = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(body)
+    };
+    callback(null, res);
+  });
 }
