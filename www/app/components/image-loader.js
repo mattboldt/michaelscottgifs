a451c9efcf00b5bc6be2a25f51a4931ac159b6a4
Matt Boldt <me@mattboldt.com>
2018-02-10 15:10:33 -0600

    added query params and metadata

--- www/app/components/image-loader.js
+++ www/app/components/image-loader.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import config from '../config/environment';
 import { bind } from '@ember/runloop';
 import $ from 'jquery';
@@ -9,21 +10,46 @@ export default Component.extend({
   searchQuery: '',
   apiUrl: config.APP.API_URL,
 
+  pageTitle: computed('query', function() {
+    const q = this.get('query');
+    if (q === 0) {
+      return "Michael Scott Gifs";
+    } else {
+      return "Michael Scott Gif search: " + q;
+    }
+  }),
+
+  pageImageUrl: computed('images', function() {
+    const images = this.get('images');
+    if (images) {
+      return images[0].url;
+    } else {
+      return '';
+    }
+  }),
+
   didInsertElement() {
     this._super(...arguments);
+    this.set('searchQuery', this.get('query'));
     this.loadImage();
   },
 
   loadImage: function() {
     const url = this.get('apiUrl') + '?query=' + this.get('searchQuery');
     $.getJSON(url).then((images) => {
-      bind(this, this.set('images', images));
+      bind(this, this.setImages(images));
     });
+  },
+
+  setImages(images) {
+    this.set('images', images);
   },
 
   actions: {
     search: function() {
-      this.loadImage(this.get('searchQuery'));
+      const query = this.get('searchQuery');
+      this.set('query', query)
+      this.loadImage(query);
     }
   }
 });
